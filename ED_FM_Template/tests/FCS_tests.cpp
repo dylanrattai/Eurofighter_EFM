e5bd35bf065b1ac3d89304960d4f2934fa6f9318
dylanrattai <dylan.rattai@ndus.edu>
2026-03-09 23:46:12 -0500

    Improved structs for FCS limits, added docs to said structs.

--- ED_FM_Template/tests/FCS_tests.cpp
+++ ED_FM_Template/tests/FCS_tests.cpp
@@ -6,95 +6,139 @@ namespace {
     constexpr double DEG_TO_RAD { 3.14159265358979323846 / 180.0 };
     constexpr double RAD_TO_DEG { 180.0 / 3.14159265358979323846 };
 
-    struct STRUCTURAL_LIMITS {
+    /**
+     * @brief Struct to hold FCS limits for different modes, to be used in tests for expected values.
+     */
+    struct Limits {
         // AOA Limits
-        static constexpr double MAX_POSITIVE_AOA_DEG { 0.0 };
-        static constexpr double MAX_NEGATIVE_AOA_DEG { 0.0 };
+        double maxPositiveAoADeg;
+        double maxNegativeAoADeg;
 
         // G Limits
-        static constexpr double MAX_POSITIVE_G { 7.0 };
-        static constexpr double MAX_NEGATIVE_G { -3.0 };
+        double maxPositiveG;
+        double maxNegativeG;
 
         // Pitch Rate Limits
-        static constexpr double MAX_POSITIVE_PITCH_RATE_DEG_PER_SEC { 0.0 };
-        static constexpr double MAX_NEGATIVE_PITCH_RATE_DEG_PER_SEC { 0.0 };
+        double maxPositivePitchRateDegPerSec;
+        double maxNegativePitchRateDegPerSec;
 
         // Roll Rate Limits
-        static constexpr double MAX_POSITIVE_ROLL_RATE_DEG_PER_SEC { 0.0 };
-        static constexpr double MAX_NEGATIVE_ROLL_RATE_DEG_PER_SEC { 0.0 };
+        double maxPositiveRollRateDegPerSec;
+        double maxNegativeRollRateDegPerSec;
     };
 
-    struct SUPERSONIC_LAW {
+    /**
+     * @brief FCS limits for the structural limits of the aircraft.
+     * 
+     * @attention TODO: THESE ARE PLACEHOLDER VALUES, ALL 0.0
+     */
+    constexpr Limits STRUCTURAL_LIMITS {
         // AOA Limits
-        static constexpr double MAX_POSITIVE_AOA_DEG { 0.0 };
-        static constexpr double MAX_NEGATIVE_AOA_DEG { 0.0 };
+        .maxPositiveAoADeg { 0.0 },
+        .maxNegativeAoADeg { 0.0 },
 
         // G Limits
-        static constexpr double MAX_POSITIVE_G { 0.0 };
-        static constexpr double MAX_NEGATIVE_G { 0.0 };
+        .maxPositiveG { 0.0 },
+        .maxNegativeG { 0.0 },
 
         // Pitch Rate Limits
-        static constexpr double MAX_POSITIVE_PITCH_RATE_DEG_PER_SEC { 0.0 };
-        static constexpr double MAX_NEGATIVE_PITCH_RATE_DEG_PER_SEC { 0.0 };
+        .maxPositivePitchRateDegPerSec { 0.0 },
+        .maxNegativePitchRateDegPerSec { 0.0 },
 
         // Roll Rate Limits
-        static constexpr double MAX_POSITIVE_ROLL_RATE_DEG_PER_SEC { 0.0 };
-        static constexpr double MAX_NEGATIVE_ROLL_RATE_DEG_PER_SEC { 0.0 };
+        .maxPositiveRollRateDegPerSec { 0.0 },
+        .maxNegativeRollRateDegPerSec { 0.0 }
     };
 
-    struct SUBSONIC_LAW {
+    /**
+     * @brief FCS limit consts for Air to Air refueling law.
+     * 
+     * @attention TODO: THESE ARE PLACEHOLDER VALUES, ALL 0.0
+     */
+    constexpr Limits REFUELING_LIMITS {
         // AOA Limits
-        static constexpr double MAX_POSITIVE_AOA_DEG { 0.0 };
-        static constexpr double MAX_NEGATIVE_AOA_DEG { 0.0 };
+        .maxPositiveAoADeg { 0.0 },
+        .maxNegativeAoADeg { 0.0 },
 
         // G Limits
-        static constexpr double MAX_POSITIVE_G { 0.0 };
-        static constexpr double MAX_NEGATIVE_G { 0.0 };
+        .maxPositiveG { 0.0 },
+        .maxNegativeG { 0.0 },
 
         // Pitch Rate Limits
-        static constexpr double MAX_POSITIVE_PITCH_RATE_DEG_PER_SEC { 0.0 };
-        static constexpr double MAX_NEGATIVE_PITCH_RATE_DEG_PER_SEC { 0.0 };
+        .maxPositivePitchRateDegPerSec { 0.0 },
+        .maxNegativePitchRateDegPerSec { 0.0 },
 
         // Roll Rate Limits
-        static constexpr double MAX_POSITIVE_ROLL_RATE_DEG_PER_SEC { 0.0 };
-        static constexpr double MAX_NEGATIVE_ROLL_RATE_DEG_PER_SEC { 0.0 };
+        .maxPositiveRollRateDegPerSec { 0.0 },
+        .maxNegativeRollRateDegPerSec { 0.0 }
     };
 
-    struct AAR_LAW {
+    /**
+     * @brief FCS limit consts for landing law.
+     * 
+     * @attention TODO: THESE ARE PLACEHOLDER VALUES, ALL 0.0
+     */
+    constexpr Limits LANDING_LIMITS {
         // AOA Limits
-        static constexpr double MAX_POSITIVE_AOA_DEG { 0.0 };
-        static constexpr double MAX_NEGATIVE_AOA_DEG { 0.0 };
+        .maxPositiveAoADeg { 0.0 },
+        .maxNegativeAoADeg { 0.0 },
 
         // G Limits
-        static constexpr double MAX_POSITIVE_G { 0.0 };
-        static constexpr double MAX_NEGATIVE_G { 0.0 };
+        .maxPositiveG { 0.0 },
+        .maxNegativeG { 0.0 },
 
         // Pitch Rate Limits
-        static constexpr double MAX_POSITIVE_PITCH_RATE_DEG_PER_SEC { 0.0 };
-        static constexpr double MAX_NEGATIVE_PITCH_RATE_DEG_PER_SEC { 0.0 };
+        .maxPositivePitchRateDegPerSec { 0.0 },
+        .maxNegativePitchRateDegPerSec { 0.0 },
 
         // Roll Rate Limits
-        static constexpr double MAX_POSITIVE_ROLL_RATE_DEG_PER_SEC { 0.0 };
-        static constexpr double MAX_NEGATIVE_ROLL_RATE_DEG_PER_SEC { 0.0 };
+        .maxPositiveRollRateDegPerSec { 0.0 },
+        .maxNegativeRollRateDegPerSec { 0.0 }
     };
 
-    struct LANDING_LAW {
+    /**
+     * @brief FCS limit consts for supersonic law.
+     * 
+     * @attention TODO: THESE ARE PLACEHOLDER VALUES, ALL 0.0
+     */
+    constexpr Limits SUBSONIC_LIMITS {
         // AOA Limits
-        static constexpr double MAX_POSITIVE_AOA_DEG { 0.0 };
-        static constexpr double MAX_NEGATIVE_AOA_DEG { 0.0 };
+        .maxPositiveAoADeg { 0.0 },
+        .maxNegativeAoADeg { 0.0 },
 
         // G Limits
-        static constexpr double MAX_POSITIVE_G { 0.0 };
-        static constexpr double MAX_NEGATIVE_G { 0.0 };
+        .maxPositiveG { 0.0 },
+        .maxNegativeG { 0.0 },
 
         // Pitch Rate Limits
-        static constexpr double MAX_POSITIVE_PITCH_RATE_DEG_PER_SEC { 0.0 };
-        static constexpr double MAX_NEGATIVE_PITCH_RATE_DEG_PER_SEC { 0.0 };
+        .maxPositivePitchRateDegPerSec { 0.0 },
+        .maxNegativePitchRateDegPerSec { 0.0 },
 
         // Roll Rate Limits
-        static constexpr double MAX_POSITIVE_ROLL_RATE_DEG_PER_SEC { 0.0 };
-        static constexpr double MAX_NEGATIVE_ROLL_RATE_DEG_PER_SEC { 0.0 };
+        .maxPositiveRollRateDegPerSec { 0.0 },
+        .maxNegativeRollRateDegPerSec { 0.0 }
     };
 
+    /**
+     * @brief FCS limit consts for supersonic law.
+     * 
+     * @attention TODO: THESE ARE PLACEHOLDER VALUES, ALL 0.0
+     */
+    constexpr Limits SUPERSONIC_LIMITS {
+        // AOA Limits
+        .maxPositiveAoADeg { 0.0 },
+        .maxNegativeAoADeg { 0.0 },
 
+        // G Limits
+        .maxPositiveG { 0.0 },
+        .maxNegativeG { 0.0 },
+
+        // Pitch Rate Limits
+        .maxPositivePitchRateDegPerSec { 0.0 },
+        .maxNegativePitchRateDegPerSec { 0.0 },
+
+        // Roll Rate Limits
+        .maxPositiveRollRateDegPerSec { 0.0 },
+        .maxNegativeRollRateDegPerSec { 0.0 }
+    };
 }
